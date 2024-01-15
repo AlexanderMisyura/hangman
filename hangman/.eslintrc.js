fb05f3d50bf6f8edfb29a87d36b22903262fe10c
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2024-01-15 03:32:05 +0300

    init: add initial config and setup files

---
+++ hangman/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2023: true,
+  },
+  extends: ['airbnb-base', 'eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2022,
+  },
+  rules: {
+    'no-plusplus': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  plugins: ['prettier'],
+  ignorePatterns: ['dist'],
+};
